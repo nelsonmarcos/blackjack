48b18c3dea8ecceed19e6ebf0e5de4084a024839
Ricardo Dias <ricardo@ricardodias.org>
2015-04-08 14:06:26 -0300

    ...

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,4 @@
-SUITS = ('C', 'S', 'H', 'D')
-RANKS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K')
-VALUES = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':10, 'Q':10, 'K':10}
+import random
 
 class Card(object):
     def __init__(self, suit, rank):
@@ -8,8 +6,23 @@ class Card(object):
         self.rank = rank
 
 class Deck(object):
+    SUITS = ('C', 'S', 'H', 'D')
+    RANKS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K')
+    VALUES = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':10, 'Q':10, 'K':10}
+
+    def __init__(self):
+        self.cards = []
+        for suit in SUITS:
+            for rank in RANKS:
+                self.cards.append(Card(suit, rank))
+
+        random.shuffle(self.cards)
+
     def get_card(self):
         return Card('C', '2')
+
+    def get_size(self):
+        return len(self.cards)
 
 class Player(object):
     pass
